0dd6fa65527419b10438ea5e592bb8a567288d8d
Phapoom Saksri <blueskychan@notmycode.dev>
2025-01-05 01:45:14 +0700

    DO NOT USE THIS, TEST ONLY

--- target/a23xq/patches/camera/customize.sh
+++ target/a23xq/patches/camera/customize.sh
@@ -179,22 +179,6 @@ done
 echo "Add stock camera libs"
 BLOBS_LIST="
 system/etc/public.libraries-camera.samsung.txt
-system/lib/libSlowShutter_jni.media.samsung.so
-system/lib64/libFace_Landmark_Engine.camera.samsung.so
-system/lib64/libHpr_RecFace_dl_v1.0.camera.samsung.so
-system/lib64/libImageCropper.camera.samsung.so
-system/lib64/libImageTagger.camera.samsung.so
-system/lib64/libMyFilter.camera.samsung.so
-system/lib64/libPortraitDistortionCorrection.arcsoft.so
-system/lib64/libSlowShutter_jni.media.samsung.so
-system/lib64/libhigh_dynamic_range.arcsoft.so
-system/lib64/libhumantracking_util.camera.samsung.so
-system/lib64/libhumantracking.arcsoft.so
-system/lib64/liblow_light_hdr.arcsoft.so
-system/lib64/libsaiv_HprFace_cmh_support_jni.camera.samsung.so
-system/lib64/libsamsung_videoengine_9_0.so
-system/lib64/libtensorflowLite.myfilter.camera.samsung.so
-system/lib64/libtensorflowlite_inference_api.myfilter.camera.samsung.so
 "
 for blob in $BLOBS_LIST
 do
