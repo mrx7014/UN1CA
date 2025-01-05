40bb7ff6d5805ab86c3b4f350e74e377faa03bb2
Phapoom Saksri <mind@blueskychan.dev>
2025-01-05 13:08:57 +0700

    I forgot something

--- target/a23xq/patches/stock_blobs/customize.sh
+++ target/a23xq/patches/stock_blobs/customize.sh
@@ -61,6 +61,28 @@ ADD_TO_WORK_DIR()
         TMP="$(dirname "$TMP")"
     done
 }
+
+REMOVE_FROM_WORK_DIR()
+{
+    local FILE_PATH="$1"
+
+    if [ -e "$FILE_PATH" ]; then
+        local FILE
+        local PARTITION
+        FILE="$(echo -n "$FILE_PATH" | sed "s.$WORK_DIR/..")"
+        PARTITION="$(echo -n "$FILE" | cut -d "/" -f 1)"
+
+        echo "Debloating /$FILE"
+        rm -rf "$FILE_PATH"
+
+        [[ "$PARTITION" == "system" ]] && FILE="$(echo "$FILE" | sed 's.^system/system/.system/.')"
+        FILE="$(echo -n "$FILE" | sed 's/\//\\\//g')"
+        sed -i "/$FILE /d" "$WORK_DIR/configs/fs_config-$PARTITION"
+
+        FILE="$(echo -n "$FILE" | sed 's/\./\\\\\./g')"
+        sed -i "/$FILE /d" "$WORK_DIR/configs/file_context-$PARTITION"
+    fi
+}
 # ]
 
 MODEL=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 1)
